145b9555bfa5b0d460edf2f0a224226ac9c69cde
MohammedAlKarasneh <101416977+MohammedAlKarasneh@users.noreply.github.com>
2022-04-07 12:55:32 +0300

    Update data.js

--- scripts/group-generator/resources/data.js
+++ scripts/group-generator/resources/data.js
@@ -12,14 +12,14 @@ const sqlResource = require('./sql');
 const dbSetupCfg = config.get('dbSetup');
 const dbPool = new DbPool(dbSetupCfg);
 
-const generateGroupSubscribers = async(groupSize, groupName) => await dbPool.executeQuery(sqlResource.dropGenerateGroupSubscribersProcedure)
-    .then(async() => await dbPool.executeQuery(sqlResource.createGenerateGroupSubscribersProcedure))
-    .then(async() => await dbPool.executeQuery(sqlResource.callGenerateGroupSubscribersProcedure, [groupSize, groupName]))
-    .then(async() => (await dbPool.executeQuery(sqlResource.selectGroupId))[0].groupId)
-    .then(async(groupId) => {
-        await dbPool.executeQuery(sqlResource.dropGenerateGroupSubscribersProcedure);
-        return groupId;
-    });
+const generateGroupSubscribers = async(groupSize, groupName) => {
+    await dbPool.executeQuery(sqlResource.dropGenerateGroupSubscribersProcedure);
+    await dbPool.executeQuery(sqlResource.createGenerateGroupSubscribersProcedure);
+    await dbPool.executeQuery(sqlResource.callGenerateGroupSubscribersProcedure, [groupSize, groupName]);
+    const groupId = (await dbPool.executeQuery(sqlResource.selectGroupId))[0].groupId;
+    await dbPool.executeQuery(sqlResource.dropGenerateGroupSubscribersProcedure);
+    return groupId;
+};
 module.exports = {
     generateGroupSubscribers,
 };
